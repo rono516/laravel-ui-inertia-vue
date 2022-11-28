0abdb06cad28bbd336badd5c8053bceb05583c3f
Collins Rono <ronocollins2000@gmail.com>
2022-11-28 10:34:26 +0300

    first commit

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -7,6 +7,7 @@
                 <div class="card">
                     <div class="card-header">Dashboard</div>
                     <div class="card-body">
+                        You have verfied your emails <br>
                         You are logged in!
                     </div>
                 </div>
